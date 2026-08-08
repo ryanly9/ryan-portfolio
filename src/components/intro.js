16d46b0b6328a89cb00cabf3a96fbcb9f0ee2912
Ryan Ahmad <112887268+ryanly9@users.noreply.github.com>
2026-08-08 15:11:03 +0530

    Update intro.js

--- src/components/intro.js
+++ src/components/intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
       <section id="intro">
         <div className="introContent">
             <span className="hello"> <b>Hello,</b> 🙋🏻</span>
-            <span className="introtext">I'm <span className="introname">Ryan Ahmed</span> <br />Aspiring Machine Learning Engineer<br /></span>
+            <span className="introtext">I'm <span className="introname">Ryan Ahmad</span> <br />Aspiring Machine Learning Engineer<br /></span>
         <p className="introPara">Masters Student in Computer Application with hands-on experience in Machine Learning, Web Development and Database Management.</p>
         {/* <Link><button className="btn"> <img src="" alt="" className='btnImg' /> </Link> */}
         <Link activeClass='active' to = 'works' spy={true} smooth={true} offset={-100} duration={500} ><button className="btn"><b>See more</b></button></Link>
